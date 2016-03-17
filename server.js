b2b1ab0ee0c6c1af106e1a4509d9cf99bea3ae98
Mike Lewis <mick.lewis84@gmail.com>
2016-03-17 22:19:04 +1100

    Add DELETE todos route

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/todos', function ( req, res ) {
 	res.json(todos);
 });
 
+// GET
 app.get( '/todos/:id', function ( req, res ) {
 	var todoId = parseInt( req.params.id, 10 );
 	// Iterate over todos array. Find a match.
@@ -29,6 +30,7 @@ app.get( '/todos/:id', function ( req, res ) {
 	} 
 });
 
+// POST
 app.post( '/todos', function ( req, res ) {
 	var body = _.pick( req.body, 'description', 'completed' );
 
@@ -43,6 +45,21 @@ app.post( '/todos', function ( req, res ) {
 
 	res.json(body);
 });
+
+// DELETE 
+app.delete( '/todos/:id', function ( req, res ) {
+	var todoId = parseInt( req.params.id, 10 );
+	var matchedTodo = _.findWhere( todos , { id: todoId } );	
+
+	if ( !matchedTodo ) {
+		res.status(404).json( {"error": "no todo found with that id" } );
+	} else {		
+		todos = _.without( todos, matchedTodo );
+		res.json( matchedTodo );
+	} 
+});
+
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!' );
