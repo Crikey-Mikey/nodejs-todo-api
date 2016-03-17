b39899fb31c1300008d75a661c7fc6358d07c9a0
Mike Lewis <mick.lewis84@gmail.com>
2016-03-17 23:28:11 +1100

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,32 @@
+// var person = {
+// 	name: 'mike',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'mike',
+// 	// 	age: 31
+// 	// };
+
+// 	obj.age = 31;
+// }
+
+// updatePerson(person);
+
+// console.log(person);
+
+
+// Array Example 
+
+grades = [15, 37];
+
+function addGrade(array) {
+	array.push(45);
+	debugger;
+}
+
+addGrade(grades);
+
+console.log(grades);
+
